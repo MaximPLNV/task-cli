b2f10d8f357273d5bedb7b0ef7e06ef7ace4f2bd
m.palynov <m.palynov@itransition.com>
2026-01-22 16:25:35 +0300

    task-cli init

---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import cmd "task-cli/internal/infrastracture/delivery"
+
+func main() {
+	cmd.Execute()
+}
---
+++ internal/usecases/task_usecase.go
@@ -0,0 +1,3 @@
+package usecases
+
+type TaskUseCase struct{}
